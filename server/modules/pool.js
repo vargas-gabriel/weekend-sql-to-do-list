54a76e5dd9f1df1c165f7a19a7d20bb8daacad15
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-09-14 18:23:13 -0500

    configure db for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,11 +1,13 @@
 const pg = require('pg');
 console.log('what is my database url', process.env.DATABASE_URL);
+//const pool = new pg.Pool({
+  //  database: 'weekend-to-do-app',
+   /// host: 'localhost',
+   // port: 5432,
+   // max: 12,
+   // idleTimeoutMillis: 300000
+//})//end db configuration
 const pool = new pg.Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 12,
-    idleTimeoutMillis: 300000
-})//end db configuration
-
+    connectionString: process.env.DATABASE_URL
+});
 module.exports = pool;
