1d95bb6186779b916c7ba97a8b93f4c8385e9643
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-09-14 18:48:14 -0500

    update pg config for localhost

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -7,7 +7,21 @@ console.log('what is my database url', process.env.DATABASE_URL);
    // max: 12,
    // idleTimeoutMillis: 300000
 //})//end db configuration
-const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
-});
+
+let pool; 
+if (process.env.DATABASE_URL) {
+    console.log('connect to a heroku db');
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL
+    });
+}
+else {
+    console.log('local db');
+    pool = new pg.Pool({
+        database: 'weekend-to-do-app'
+    });
+
+}
+
+
 module.exports = pool;
