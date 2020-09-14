05999d89e86ddb105dcf2d32bd48641c24e3e64e
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-09-14 18:03:01 -0500

    heroku port

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use('/tasks', tasks);
 
 // globals
-const port = 3000;
+const port = process.env.PORT || 3000;
+console.log('Heroku port is:', port);
 
 // spin up server
 app.listen( port, ()=>{
